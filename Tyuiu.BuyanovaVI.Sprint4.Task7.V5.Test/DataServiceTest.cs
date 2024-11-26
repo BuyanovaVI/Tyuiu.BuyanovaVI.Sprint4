a74f7be9cab210419c1762e9638ffdf75bc425ea
Tyuiu.BuynovaVI@outlook.com <v.buyanova1@mail.ru>
2024-11-26 21:27:11 +0500

    Task7

---
+++ Tyuiu.BuyanovaVI.Sprint4.Task7.V5.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+using Tyuiu.BuyanovaVI.Sprint4.Task7.V5.Lib;
+
+namespace Tyuiu.BuyanovaVI.Sprint4.Task7.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string array = "123456789";
+            int wail = 4;
+            int a = 3;
+            int b = 3;
+            int res = ds.Calculate(a, b, array);
+            Assert.AreEqual(wail, res);
+        }
+    }
+}
